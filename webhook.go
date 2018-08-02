38412163ec3757f820eb4faebff97071f18109c6
Baptiste Jamin <baptistejamin@gmail.com>
2018-08-02 17:50:14 +0200

    Fix

--- webhook.go
+++ webhook.go
@@ -74,29 +74,29 @@ func (a TwitterApi) PutActivityWebhooks(v url.Values, webhookID string, env stri
 //SetWHSubscription Subscribes the provided app to events for the provided user context.
 //When subscribed, all DM events for the provided user will be sent to the app’s webhook via POST request.
 //https://dev.twitter.com/webhooks/reference/post/account_activity/webhooks/subscriptions
-func (a TwitterApi) SetWHSubscription(v url.Values, webhookID string, env string) (u interface{}, err error) {
+func (a TwitterApi) SetWHSubscription(v url.Values, env string) (u interface{}, err error) {
 	v = cleanValues(v)
 	responseCh := make(chan response)
-	a.queryQueue <- query{a.baseUrl + "/account_activity/all/" + env + "/" + webhookID + "/subscriptions.json", v, &u, _POST, responseCh}
+	a.queryQueue <- query{a.baseUrl + "/account_activity/all/" + env + "/subscriptions.json", v, &u, _POST, responseCh}
 	return u, (<-responseCh).err
 }
 
 //GetWHSubscription Provides a way to determine if a webhook configuration is
 //subscribed to the provided user’s Direct Messages.
 //https://dev.twitter.com/webhooks/reference/get/account_activity/webhooks/subscriptions
-func (a TwitterApi) GetWHSubscription(v url.Values, webhookID string, env string) (u interface{}, err error) {
+func (a TwitterApi) GetWHSubscription(v url.Values, env string) (u interface{}, err error) {
 	v = cleanValues(v)
 	responseCh := make(chan response)
-	a.queryQueue <- query{a.baseUrl + "/account_activity/all/" + env + "/" + webhookID + "/subscriptions.json", v, &u, _GET, responseCh}
+	a.queryQueue <- query{a.baseUrl + "/account_activity/all/" + env + "/subscriptions.json", v, &u, _GET, responseCh}
 	return u, (<-responseCh).err
 }
 
 //DeleteWHSubscription Deactivates subscription for the provided user context and app. After deactivation,
 //all DM events for the requesting user will no longer be sent to the webhook URL..
 //https://dev.twitter.com/webhooks/reference/del/account_activity/webhooks
-func (a TwitterApi) DeleteWHSubscription(v url.Values, webhookID string, env string) (u interface{}, err error) {
+func (a TwitterApi) DeleteWHSubscription(v url.Values, env string) (u interface{}, err error) {
 	v = cleanValues(v)
 	responseCh := make(chan response)
-	a.queryQueue <- query{a.baseUrl + "/account_activity/all/" + env + "/" + webhookID + "/subscriptions.json", v, &u, _DELETE, responseCh}
+	a.queryQueue <- query{a.baseUrl + "/account_activity/all/" + env + "/subscriptions.json", v, &u, _DELETE, responseCh}
 	return u, (<-responseCh).err
 }
